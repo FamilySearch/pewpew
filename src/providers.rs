FamilySearch/pewpew

commit d07440804bbae715ca952620e38b9e2c6e75867a

Refactor and update tests

src/providers.rs
@@ -10,13 +10,12 @@ use crate::load_test::TestEndReason;
 use crate::util::{json_value_into_string, tweak_path};
 
 use channel::Limit;
-use either::{Either, Either3};
-use futures::{stream, sync::mpsc::Sender as FCSender, Future, Stream};
+use ether::{Either, Either3};
+use futures::{stream, sync::mpsc::Sender as FCSender, Future, IntoFuture, Sink, Stream};
 use serde_json as json;
-use tokio::{fs::File as TokioFile, prelude::*};
 use tokio_threadpool::blocking;
 
-use std::{io, path::PathBuf, sync::Arc};
+use std::{io, path::PathBuf};
 
 pub struct Provider {
     pub auto_return: Option<config::EndpointProvidesSendOptions>,
@@ -33,9 +32,9 @@ impl Provider {
     ) -> Self {
         Provider {
             auto_return,
+            on_demand: channel::OnDemandReceiver::new(&rx),
+            rx,
             tx,
-            rx: rx.clone(),
-            on_demand: channel::OnDemandReceiver::new(rx),
         }
     }
 }
@@ -95,39 +94,13 @@ pub fn file(
     Ok(Provider::new(template.auto_return, rx, tx))
 }
 
-#[must_use = "streams do nothing unless polled"]
-struct RepeaterStream<T> {
-    i: usize,
-    values: Vec<T>,
-}
-
-impl<T> RepeaterStream<T> {
-    fn new(values: Vec<T>) -> Self {
-        RepeaterStream { i: 0, values }
-    }
-}
-
-impl<T: Clone> Stream for RepeaterStream<T> {
-    type Item = T;
-    type Error = channel::ChannelClosed;
-
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        let i = self.i;
-        self.i = (self.i + 1).checked_rem(self.values.len()).unwrap_or(0);
-        Ok(Async::Ready(self.values.get(i).cloned()))
-    }
-}
-
 pub fn response(template: config::ResponseProvider) -> Provider {
     let (tx, rx) = channel::channel(template.buffer);
     Provider::new(template.auto_return, rx, tx)
 }
 
-pub fn literals(
-    values: Vec<json::Value>,
-    auto_return: Option<config::EndpointProvidesSendOptions>,
-) -> Provider {
-    let rs: RepeaterStream<json::Value> = RepeaterStream::new(values);
+pub fn literals(values: Vec<json::Value>) -> Provider {
+    let rs = stream::iter_ok::<_, channel::ChannelClosed>(values.into_iter().cycle());
     let (tx, rx) = channel::channel(Limit::auto());
     let tx2 = tx.clone();
     let prime_tx = rs
@@ -135,13 +108,12 @@ pub fn literals(
         // Error propagate here when sender channel closes at test conclusion
         .then(|_| Ok(()));
     tokio::spawn(prime_tx);
-    Provider::new(auto_return, rx, tx)
+    Provider::new(None, rx, tx)
 }
 
 pub fn range(range: config::RangeProvider) -> Provider {
     let (tx, rx) = channel::channel(Limit::auto());
     let prime_tx = stream::iter_ok::<_, channel::ChannelClosed>(range.0.map(json::Value::from))
-        // .map_err(|_| channel::ChannelClosed)
         .forward(tx.clone())
         // Error propagate here when sender channel closes at test conclusion
         .then(|_| Ok(()));
@@ -149,31 +121,45 @@ pub fn range(range: config::RangeProvider) -> Provider {
     Provider::new(None, rx, tx)
 }
 
-pub fn logger(
-    mut template: config::Logger,
+pub fn logger<F, W>(
+    name: String,
+    template: config::Logger,
     test_killer: FCSender<Result<TestEndReason, TestError>>,
-    config_path: &PathBuf,
-) -> channel::Sender<json::Value> {
+    writer_future: F,
+) -> channel::Sender<json::Value>
+where
+    F: Future<Item = W, Error = TestError> + Send + Sync + 'static,
+    W: io::Write + Send + Sync + 'static,
+{
     let (tx, rx) = channel::channel::<json::Value>(Limit::Integer(5));
     let limit = template.limit;
     let pretty = template.pretty;
     let kill = template.kill;
     let mut counter = 0;
     let mut keep_logging = true;
-    match template.to.as_str() {
-        "stderr" => {
-            let logger = rx
+    let test_killer2 = test_killer.clone();
+    let logger = writer_future
+        .and_then(move |mut writer| {
+            rx.map_err(|_| TestError::Internal("logger receiver unexpectedly errored".into()))
                 .for_each(move |v| {
                     counter += 1;
-                    if keep_logging {
-                        if pretty && !v.is_string() {
-                            eprintln!("{:#}", v);
+                    let result = if keep_logging {
+                        if pretty {
+                            writeln!(writer, "{:#}", v)
                         } else {
-                            eprintln!("{}", json_value_into_string(v));
+                            writeln!(writer, "{}", json_value_into_string(v))
                         }
-                    }
+                    } else {
+                        Ok(())
+                    };
+                    let result = result
+                        .map_err(|e| {
+                            TestError::Other(format!("writing to logger `{}`: {}", name, e).into())
+                        })
+                        .into_future();
                     match limit {
                         Some(limit) if counter >= limit => {
+                            keep_logging = false;
                             if kill {
                                 Either3::B(
                                     test_killer
@@ -182,120 +168,25 @@ pub fn logger(
                                         .then(|_| Ok(())),
                                 )
                             } else {
-                                keep_logging = false;
-                                Either3::A(Ok(()).into_future())
+                                Either3::A(result)
                             }
                         }
-                        None if kill => Either3::C(
-                            test_killer
-                                .clone()
-                                .send(Err(TestError::KilledByLogger))
-                                .then(|_| Ok(())),
-                        ),
-                        _ => Either3::A(Ok(()).into_future()),
-                    }
-                })
-                .then(|_| Ok(()));
-            tokio::spawn(logger);
-        }
-        "stdout" => {
-            let logger = rx
-                .for_each(move |v| {
-                    counter += 1;
-                    if keep_logging {
-                        if pretty && !v.is_string() {
-                            println!("{:#}", v);
-                        } else {
-                            println!("{}", json_value_into_string(v));
-                        }
-                    }
-                    match limit {
-                        Some(limit) if counter >= limit => {
-                            if kill {
-                                Either3::B(
-                                    test_killer
-                                        .clone()
-                                        .send(Err(TestError::KilledByLogger))
-                                        .then(|_| Ok(())),
-                                )
-                            } else {
-                                keep_logging = false;
-                                Either3::A(Ok(()).into_future())
-                            }
-                        }
-                        None if kill => Either3::C(
-                            test_killer
-                                .clone()
-                                .send(Err(TestError::KilledByLogger))
-                                .then(|_| Ok(())),
-                        ),
-                        _ => Either3::A(Ok(()).into_future()),
-                    }
-                })
-                .then(|_| Ok(()));
-            tokio::spawn(logger);
-        }
-        _ => {
-            tweak_path(&mut template.to, config_path);
-            let file_name = Arc::new(template.to);
-            let file_name2 = file_name.clone();
-            let test_killer2 = test_killer.clone();
-            let logger = TokioFile::create((&*file_name).clone())
-                .map_err(move |e| {
-                    TestError::Other(
-                        format!("creating logger file `{:?}`: {}", file_name2, e).into(),
-                    )
-                })
-                .and_then(move |mut file| {
-                    rx.map_err(|_| {
-                        TestError::Internal("logger receiver unexpectedly errored".into())
-                    })
-                    .for_each(move |v| {
-                        let file_name = file_name.clone();
-                        counter += 1;
-                        let result = if keep_logging {
-                            if pretty {
-                                writeln!(file, "{:#}", v)
-                            } else {
-                                writeln!(file, "{}", v)
-                            }
-                        } else {
-                            Ok(())
-                        };
-                        let result = result.into_future().map_err(move |e| {
-                            TestError::Other(
-                                format!("writing to file `{}`: {}", file_name, e).into(),
-                            )
-                        });
-                        match limit {
-                            Some(limit) if counter >= limit => {
-                                if kill {
-                                    Either3::B(
-                                        test_killer
-                                            .clone()
-                                            .send(Err(TestError::KilledByLogger))
-                                            .then(|_| Ok(())),
-                                    )
-                                } else {
-                                    keep_logging = false;
-                                    Either3::A(result)
-                                }
-                            }
-                            None if kill => Either3::C(
+                        None if kill => {
+                            keep_logging = false;
+                            Either3::C(
                                 test_killer
                                     .clone()
                                     .send(Err(TestError::KilledByLogger))
                                     .then(|_| Ok(())),
-                            ),
-                            _ => Either3::A(result),
-                        }
-                    })
+                            )
+                        },
+                        _ => Either3::A(result),
+                    }
                 })
-                .or_else(move |e| test_killer2.send(Err(e)).then(|_| Ok::<_, ()>(())))
-                .then(|_| Ok(()));
-            tokio::spawn(logger);
-        }
-    }
+        })
+        .or_else(move |e| test_killer2.send(Err(e)).then(|_| Ok::<_, ()>(())))
+        .then(|_| Ok(()));
+    tokio::spawn(logger);
     tx
 }
 
@@ -305,4 +196,253 @@ fn into_stream<I: Iterator<Item = Result<json::Value, io::Error>>>(
     stream::poll_fn(move || blocking(|| iter.next()))
         .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
         .and_then(|r| r)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::{
+        future,
+        sync::mpsc::channel as futures_channel,
+        Async,
+    };
+    use json::json;
+    use parking_lot::Mutex;
+    use tokio::runtime::current_thread;
+
+    use std::{
+        sync::Arc,
+        time::{Duration, Instant},
+    };
+
+    #[test]
+    fn range_provider_works() {
+        current_thread::run(future::lazy(|| {
+            let range_params = r#"{
+                "start": 0,
+                "end": 20
+            }"#;
+            let range_params = serde_json::from_str(range_params).unwrap();
+            let p = range(range_params);
+            let expects = stream::iter_ok(0..=20);
+
+            let f = p.rx.zip(expects).for_each(|(left, right)| {
+                assert_eq!(left, right);
+                Ok(())
+            });
+            current_thread::spawn(f);
+
+            let range_params = r#"{
+                "start": 0,
+                "end": 20,
+                "step": 2
+            }"#;
+            let range_params = serde_json::from_str(range_params).unwrap();
+            let p = range(range_params);
+            let expects = stream::iter_ok((0..=20).step_by(2));
+
+            let f = p.rx.zip(expects).for_each(|(left, right)| {
+                assert_eq!(left, right);
+                Ok(())
+            });
+            current_thread::spawn(f);
+
+            let range_params = r#"{
+                "start": 0,
+                "end": 20,
+                "repeat": true
+            }"#;
+
+            let range_params = serde_json::from_str(range_params).unwrap();
+            let p = range(range_params);
+            let expects = stream::iter_ok((0..=20).cycle());
+
+            p.rx.zip(expects).take(100).for_each(|(left, right)| {
+                assert_eq!(left, right);
+                Ok(())
+            })
+        }));
+    }
+
+    #[test]
+    fn literals_provider_works() {
+        current_thread::run(future::lazy(|| {
+            let jsons = vec![json!(1), json!(2), json!(3)];
+            let p = literals(jsons.clone());
+            let expects = stream::iter_ok(jsons.into_iter().cycle());
+
+            p.rx.zip(expects).take(50).for_each(|(left, right)| {
+                assert_eq!(left, right);
+                Ok(())
+            })
+        }));
+    }
+
+    #[test]
+    fn response_provider_works() {
+        current_thread::run(future::lazy(|| {
+            let jsons = vec![json!(1), json!(2), json!(3)];
+            let rp = config::ResponseProvider {
+                auto_return: None,
+                buffer: Limit::auto(),
+            };
+            let p = response(rp);
+            let responses = stream::iter_ok(jsons.clone().into_iter().cycle());
+            current_thread::spawn(p.tx.send_all(responses).then(|_| Ok(())));
+
+            let expects = stream::iter_ok(jsons.into_iter().cycle());
+
+            p.rx.zip(expects).take(50).for_each(|(left, right)| {
+                assert_eq!(left, right);
+                Ok(())
+            })
+        }));
+    }
+
+    #[derive(Clone)]
+    struct TestWriter(Arc<Mutex<Vec<u8>>>);
+
+    impl TestWriter {
+        fn new() -> Self {
+            TestWriter(Mutex::new(Vec::new()).into())
+        }
+
+        fn get_string(&self) -> String {
+            String::from_utf8(self.0.lock().split_off(0)).unwrap()
+        }
+    }
+
+    impl io::Write for TestWriter {
+        fn write(&mut self, buf: &[u8]) -> std::result::Result<usize, std::io::Error> {
+            self.0.lock().write(buf)
+        }
+
+        fn flush(&mut self) -> std::result::Result<(), std::io::Error> {
+            io::Write::flush(&mut *self.0.lock())
+        }
+    }
+
+    #[test]
+    fn basic_logger_works() {
+        current_thread::run(future::lazy(|| {
+            let logger_params = r#"{
+                "to": "",
+                "kill": true
+            }"#;
+            let logger_params = serde_json::from_str(logger_params).unwrap();
+            let (test_killer, mut test_killed_rx) = futures_channel(1);
+            let writer = TestWriter::new();
+            let writer_future = future::ok(writer.clone());
+
+            let tx = logger("".into(), logger_params, test_killer.clone(), writer_future);
+
+            tokio::spawn(
+                tx.send_all(stream::iter_ok(vec!(json!(1), json!(2))))
+                    .then(|_| Ok(()))
+            );
+
+            let f = tokio::timer::Delay::new(Instant::now() + Duration::from_millis(100))
+                .then(move |_| {
+                    let left = writer.get_string();
+                    let right = "1\n";
+                    assert_eq!(left, right, "value in writer should match");
+                    
+                    let check = if let Ok(Async::Ready(Some(Err(_)))) = test_killed_rx.poll() {
+                        true
+                    } else {
+                        false
+                    };
+                    assert!(check, "test should be killed");
+                    drop(test_killer);
+                    Ok(())
+                });
+
+            tokio::spawn(f);
+
+            Ok(())
+        }));
+    }
+
+    #[test]
+    fn logger_limit_works() {
+        current_thread::run(future::lazy(|| {
+            let logger_params = r#"{
+                "to": "",
+                "limit": 1
+            }"#;
+            let logger_params = serde_json::from_str(logger_params).unwrap();
+            let (test_killer, mut test_killed_rx) = futures_channel(1);
+            let writer = TestWriter::new();
+            let writer_future = future::ok(writer.clone());
+
+            let tx = logger("".into(), logger_params, test_killer.clone(), writer_future);
+
+            tokio::spawn(
+                tx.send_all(stream::iter_ok(vec!(json!(1), json!(2))))
+                    .then(|_| Ok(()))
+            );
+
+            let f = tokio::timer::Delay::new(Instant::now() + Duration::from_millis(100))
+                .then(move |_| {
+                    let left = writer.get_string();
+                    let right = "1\n";
+                    assert_eq!(left, right, "value in writer should match");
+                    
+                    let check = if let Ok(Async::NotReady) = test_killed_rx.poll() {
+                        true
+                    } else {
+                        false
+                    };
+                    assert!(check, "test should not be killed");
+                    drop(test_killer);
+                    Ok(())
+                });
+
+            tokio::spawn(f);
+
+            Ok(())
+        }));
+    }
+
+    #[test]
+    fn logger_pretty_works() {
+        current_thread::run(future::lazy(|| {
+            let logger_params = r#"{
+                "to": "",
+                "pretty": true
+            }"#;
+            let logger_params = serde_json::from_str(logger_params).unwrap();
+            let (test_killer, mut test_killed_rx) = futures_channel(1);
+            let writer = TestWriter::new();
+            let writer_future = future::ok(writer.clone());
+
+            let tx = logger("".into(), logger_params, test_killer.clone(), writer_future);
+
+            tokio::spawn(
+                tx.send_all(stream::iter_ok(vec!(json!({"foo": [1, 2, 3]}), json!(2))))
+                    .then(|_| Ok(()))
+            );
+
+            let f = tokio::timer::Delay::new(Instant::now() + Duration::from_millis(100))
+                .then(move |_| {
+                    let left = writer.get_string();
+                    let right = "{\n  \"foo\": [\n    1,\n    2,\n    3\n  ]\n}\n2\n";
+                    assert_eq!(left, right, "value in writer should match");
+                    
+                    let check = if let Ok(Async::NotReady) = test_killed_rx.poll() {
+                        true
+                    } else {
+                        false
+                    };
+                    assert!(check, "test should not be killed");
+                    drop(test_killer);
+                    Ok(())
+                });
+
+            tokio::spawn(f);
+
+            Ok(())
+        }));
+    }
+
 }

Cargo.toml
@@ -13,7 +13,7 @@ clap = "2"
 crossbeam = "0.7"
 csv = "1"
 chrono = "0.4"
-either = { path = "./lib/either" }
+ether = { path = "./lib/either" }
 fnv = "1"
 for_each_parallel = { path = "./lib/for_each_parallel" }
 futures = "0.1"

lib/body_reader/Cargo.toml
@@ -3,7 +3,13 @@ name = "body_reader"
 version = "0.0.0"
 edition = "2018"
 
+[lib]
+doctest = false
+
 [dependencies]
 bytes = "0.4"
 brotli-decompressor = "2"
 libflate = "0.1"
+
+[dev-dependencies]
+brotli = "3"

lib/body_reader/src/lib.rs
@@ -118,3 +118,72 @@ impl BodyReader {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{cmp, io::Write};
+
+    static TRUTH: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    trait IntoInner: Write {
+        fn into_inner(self: Box<Self>) -> Vec<u8>;
+    }
+
+    impl IntoInner for ::brotli::CompressorWriter<Vec<u8>> {
+        fn into_inner(self: Box<Self>) -> Vec<u8> {
+            ::brotli::CompressorWriter::into_inner(*self)
+        }
+    }
+
+    impl IntoInner for libflate::gzip::Encoder<Vec<u8>> {
+        fn into_inner(self: Box<Self>) -> Vec<u8> {
+            self.finish().unwrap().0
+        }
+    }
+
+    impl IntoInner for libflate::deflate::Encoder<Vec<u8>> {
+        fn into_inner(self: Box<Self>) -> Vec<u8> {
+            self.finish().unwrap().0
+        }
+    }
+
+    impl IntoInner for Vec<u8> {
+        fn into_inner(self: Box<Self>) -> Vec<u8> {
+            *self
+        }
+    }
+
+    #[test]
+    fn body_reader_works() {
+        let brotli = ::brotli::CompressorWriter::new(Vec::new(), 4096, 11, 22);
+        let gzip = libflate::gzip::Encoder::new(Vec::new()).unwrap();
+        let deflate = libflate::deflate::Encoder::new(Vec::new());
+
+        let flavors: Vec<(Box<dyn IntoInner>, &str)> = vec![
+            (Box::new(brotli), "br"),
+            (Box::new(gzip), "gzip"),
+            (Box::new(deflate), "deflate"),
+            (Box::new(Vec::new()), ""),
+        ];
+
+        for (i, (mut writer, compression)) in flavors.into_iter().enumerate() {
+            let input: Bytes = {
+                writer.write_all(TRUTH.as_bytes()).unwrap();
+                let vec = writer.into_inner();
+                vec.into()
+            };
+
+            let compression = Compression::try_from(compression).unwrap();
+            let mut reader = BodyReader::new(compression);
+            let mut decoded_bytes = BytesMut::new();
+            for n in (0..input.len()).step_by(4) {
+                let slice = input.slice(n, cmp::min(n + 4, input.len()));
+                reader.decode(slice, &mut decoded_bytes).unwrap();
+            }
+            let decoded_bytes = decoded_bytes.freeze();
+            let left = std::str::from_utf8(&decoded_bytes).unwrap();
+            assert_eq!(left, TRUTH, "index {}", i);
+        }
+    }
+}

lib/channel/Cargo.toml
@@ -3,6 +3,9 @@ name = "channel"
 version = "0.0.0"
 edition = "2018"
 
+[lib]
+doctest = false
+
 [dependencies]
 futures = "0.1"
 crossbeam-queue = "0.1"

commit 755328ef6a44268fc229310e96eb77f1c306eb0b

Make stand alone strings log without the quotes and other character escapes. Regression from d07440804bbae715ca952620e38b9e2c6e75867a

src/providers.rs
@@ -144,7 +144,7 @@ where
                 .for_each(move |v| {
                     counter += 1;
                     let result = if keep_logging {
-                        if pretty {
+                        if pretty && !v.is_string() {
                             writeln!(writer, "{:#}", v)
                         } else {
                             writeln!(writer, "{}", json_value_into_string(v))
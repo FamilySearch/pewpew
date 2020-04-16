b7af0dda59ef01d400fde1286f948495e6727b33
Eli Snow <EliSnow@users.noreply.github.com>
2020-04-16 09:33:10 -0600

    Update script for building the guide

--- guide/update-guide.sh
+++ guide/update-guide.sh
@@ -12,7 +12,7 @@ WASM_OUTPUT_DIR=$RESULTS_VIEWER_DIR/lib/hdr-histogram-wasm
 cd $WASM_LIB_DIR
 wasm-pack build --release -t web -d $WASM_OUTPUT_DIR
 cd $WASM_OUTPUT_DIR
-sed 's/module = import\.meta\.url.*/import(".\/hdr_histogram_wasm_bg.wasm");\nmodule = require.resolve(".\/hdr_histogram_wasm_bg.wasm")[0][0];/' hdr_histogram_wasm.js > hdr_histogram_wasm2.js
+sed 's/input = import\.meta\.url.*/import(".\/hdr_histogram_wasm_bg.wasm");\ninput = require.resolve(".\/hdr_histogram_wasm_bg.wasm")[0][0];/' hdr_histogram_wasm.js > hdr_histogram_wasm2.js
 mv hdr_histogram_wasm2.js hdr_histogram_wasm.js
 
 # build the results viewer (which includes putting the output into the book's src)

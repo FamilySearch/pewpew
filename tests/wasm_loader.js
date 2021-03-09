d2fcaaa7985fd707f8b96f7e06c059019edf8a5e
Trevor McMaster <trevor.mcmaster@familysearch.org>
2021-03-09 10:13:05 -0700

    Added test files used for debugging epoch in logger name

---
+++ tests/wasm_loader.js
@@ -0,0 +1,31 @@
+const _fs = require("fs");
+const { promisify } = require("util");
+const { Config } = require("./pkg/config_wasm.js");
+
+const  readFile = promisify(_fs.readFile);
+
+if (process.argv.length < 3) {
+  console.error(`Must pass a file to parse: "${process.argv.join(" ")}"`)
+  process.exit(1);
+}
+const filepath = process.argv[2];
+console.log(`filepath: "${filepath}"`);
+
+(async () => {
+  try {
+    const fileBuffer = await readFile(filepath);
+    const varMap = new Map();
+    const config = new Config(fileBuffer, varMap);
+    console.log("config loaded");
+    config.checkOk();
+    console.log("config.checkOk()");
+    console.log(`config.getBucketSize(): ${config.getBucketSize()}`);
+    console.log(`config.getDuration(): ${config.getDuration()}`);
+    console.log(`config.getInputFiles(): ${JSON.stringify(config.getInputFiles())}`);
+    console.log(`config.getLoggerFiles(): ${JSON.stringify(config.getLoggerFiles())}`);
+
+  } catch (error) {
+    console.error("Error loading config", error);
+    process.exit(1);
+  }
+})().catch((error) => console.error("Global Catch", error));

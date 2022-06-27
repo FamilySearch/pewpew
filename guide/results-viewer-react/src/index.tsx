122ac0ebab2bedf54cdb23162e4124c5495a4d3f
Trevor McMaster <trevor.mcmaster@familysearch.org>
2022-06-27 15:19:23 -0600

    Renamed App to ResultsViewer

--- guide/results-viewer-react/src/index.tsx
+++ guide/results-viewer-react/src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ResultsViewer } from "./App";
+import { ResultsViewer } from "./ResultsViewer";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root")!);

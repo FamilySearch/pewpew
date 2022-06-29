d36f296fb5155eacb0b75eb5ddceb05ae8fa2542
Trevor McMaster <trevor.mcmaster@familysearch.org>
2022-06-29 08:32:56 -0600

    Moved the createRoot back to the div since body broke our modals

--- guide/results-viewer-react/src/index.tsx
+++ guide/results-viewer-react/src/index.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import { ResultsViewer } from "./ResultsViewer";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector("body")!);
+const root = createRoot(document.querySelector("#root") || document.querySelector("div")!);
 root.render(<ResultsViewer />);

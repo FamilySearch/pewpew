b0dd53463c502470cf1c1baf0c3ccd87a7d96fe2
Trevor McMaster <trevor.mcmaster@familysearch.org>
2022-10-03 07:40:12 -0600

    Updated version for release (#100)
    * Updated version for release
    
    * Added duration changes to the README
    
    * Updated the release script to add a -d option

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-set -x
+set +x
 
 printusage() {
   if ! [[ -z "$1" ]] ; then
@@ -11,16 +11,29 @@ printusage() {
   exit 1
 }
 
+if [ "$1" == "-d" ] || [ "$1" == "delete" ] ; then
+  shift;
+  export DELETE="true"
+fi
+
 if [[ -z "$1" ]] ; then
   printusage Must provide a tag version
 fi
 
+if [[ ! -z "$DELETE" ]] ; then
+  echo "git tag -d \"$1\""
+  git tag -d "$1"
+  echo "git push --delete origin \"$1\""
+  git push --delete origin "$1"
+fi
 
-echo git tag -d "$1"
-echo git push --delete origin "$1"
+echo "git tag -a \"$1\" -m \"$1
+
+${@:2}\""
 
 git tag -a "$1" -m "$1
 
 ${@:2}"
 
+echo "git push origin \"$1\""
 git push origin "$1"

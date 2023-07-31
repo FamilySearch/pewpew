670ab744085ecf31e2aadaf3732f17edef090132
Zachery Olson <Zachery.Olson@FamilySearch.org>
2023-07-31 11:51:21 -0600

    update dependency tables to use workspace inheritance

--- lib/config-gen/src/lib.rs
+++ lib/config-gen/src/lib.rs
@@ -5,6 +5,7 @@ pub type LoadTest = config::LoadTest<False, False>;
 
 #[wasm_bindgen]
 pub fn load_test_yaml_from_js(data: &str) -> Result<String, JsError> {
+    // had some issues getting direct conversion from JsValue, so stringified JSON is used
     console_error_panic_hook::set_once();
     let lt: LoadTest = serde_json::from_str(data)?;
     let yaml = serde_yaml::to_string(&lt)?;

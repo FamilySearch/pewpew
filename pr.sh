324631261c4fd2b4b25f7ec68cc3761bb8669080
Trevor McMaster <trevor.mcmaster@familysearch.org>
2023-08-11 14:43:41 -0600

    Added config-gen, config-updater to the pr.sh script

--- pr.sh
+++ pr.sh
@@ -7,6 +7,8 @@ cargo build
 # cargo build --target x86_64-unknown-linux-musl
 # cross build --target aarch64-unknown-linux-musl
 # cross build --target armv7-unknown-linux-musleabihf
+cargo build --bin test-server
+cargo build -p pewpew-config-updater
 
 # cargo fmt --all
 cargo fmt --all -- --check
@@ -26,6 +28,15 @@ wasm-pack build --release -t nodejs --scope fs
 cd tests/
 npm ci
 npm test
+
+cd "$CWD/lib/config-gen"
+# cargo install wasm-pack
+wasm-pack build --release -t nodejs --scope fs
+# ~/.cache/.wasm-pack/wasm-opt-4d7a65327e9363b7/wasm-opt pkg/config_wasm_bg.wasm -o pkg/config_wasm_bg.wasm -Oz
+
+# cd tests/
+# npm ci
+# npm test
 
 cd "$CWD/lib/hdr-histogram-wasm"
 wasm-pack build --release -t nodejs --scope fs

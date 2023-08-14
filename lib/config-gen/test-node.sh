3a5bace93e2e86afd9196624eda9b1176fbf1c3b
Trevor McMaster <trevor.mcmaster@familysearch.org>
2023-08-14 14:22:20 -0600

    Added JS unit tests for config-gen

---
+++ lib/config-gen/test-node.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+### To build, first install wasm-pack, then run this script
+# cargo install wasm-pack
+set -e
+set -x
+
+wasm-pack test --node
+wasm-pack build --release -t nodejs --scope fs
+cd tests/
+npm install
+npm test
+cd ..

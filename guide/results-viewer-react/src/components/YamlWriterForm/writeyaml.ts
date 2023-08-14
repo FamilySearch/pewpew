df03d2f6bd922c8aab663e7e2d9bb5f3111889a1
Trevor McMaster <trevor.mcmaster@familysearch.org>
2023-08-14 09:06:10 -0600

    Removed references to SystemTest

--- guide/results-viewer-react/src/components/YamlWriterForm/writeyaml.ts
+++ guide/results-viewer-react/src/components/YamlWriterForm/writeyaml.ts
@@ -45,7 +45,7 @@ export const createYamlJson = ({ urls, patterns, vars, providers, loggers }: Omi
   // Default config
   myYaml.config = {};
   myYaml.config.client = {};
-  myYaml.config.client.headers = { "User-Agent": "FS-QA-SystemTest" };
+  myYaml.config.client.headers = { "User-Agent": "Pewpew Performance Load Test" };
   // eslint-disable-next-line camelcase
   myYaml.config.general = { bucket_size: "1m" };
 

fded509d290f65a76f29027fc765cf4a2534376a
Trevor McMaster <trevor.mcmaster@familysearch.org>
2023-09-11 15:20:15 -0600

    Added code to the pr script to build the hdr-histogram for the controller

--- pr.sh
+++ pr.sh
@@ -2,6 +2,12 @@
 set -e
 set -x
 
+PROJECT_ROOT=$(realpath ./)
+CONTROLLER_DIR=$(realpath $PROJECT_ROOT/controller)
+WASM_LIB_DIR=$(realpath $PROJECT_ROOT/lib/hdr-histogram-wasm)
+mkdir -p "$CONTROLLER_DIR/lib/hdr-histogram-wasm"
+WASM_OUTPUT_CONTROLLER_DIR=$CONTROLLER_DIR/lib/hdr-histogram-wasm
+
 cargo build
 # cargo install cross
 # cargo build --target x86_64-unknown-linux-musl
@@ -29,6 +35,7 @@ npm test
 
 cd "$CWD/lib/hdr-histogram-wasm"
 wasm-pack build --release -t nodejs --scope fs
+wasm-pack build --release -t bundler -d $WASM_OUTPUT_CONTROLLER_DIR --scope fs
 # ~/.cache/.wasm-pack/wasm-opt-4d7a65327e9363b7/wasm-opt pkg/hdr_histogram_wasm_bg.wasm -o pkg/hdr_histogram_wasm_bg.wasm -Oz
 cd tests/
 npm ci

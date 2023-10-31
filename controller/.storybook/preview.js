bcbb1eb60392ba05dd6bad81c8c790cfe99419db
Trevor McMaster <trevor.mcmaster@familysearch.org>
2023-10-31 14:10:39 -0600

    Turned off fail-fast for the Rest cross os tests (#172) (#173)
    * Turned off fail-fast for the Rest cross os tests
    
    * Updated storybook and fixed issue with next.js
    
    - https://github.com/storybookjs/storybook/issues/24234
    
    * Updated version and depencies for PPaaS

--- controller/.storybook/preview.js
+++ controller/.storybook/preview.js
@@ -1,5 +1,5 @@
 // https://www.npmjs.com/package/storybook-addon-next-router
-import { RouterContext } from "next/dist/shared/lib/router-context"; // next 12
+import { RouterContext } from "next/dist/shared/lib/router-context.shared-runtime"; // next 13.5
 
 // https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#default-export-in-previewjs
 /** @type { import('@storybook/react').Preview } */

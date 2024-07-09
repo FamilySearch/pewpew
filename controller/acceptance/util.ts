afbb5d9d713eb05bea98b1c1386fb5332f53e8e5
Trevor McMaster <trevor.mcmaster@familysearch.org>
2024-07-09 11:36:58 -0700

    Extended acceptance tests AWS (#233)
    * Moved some of the shared acceptance code into a util file.
    
    * Added code to upload files for integration and acceptance tests
    
    * Added code to upload files needed for acceptance tests
    
    * Cleaned up files
    
    * Cleaned up files
    
    * Simplified test more

---
+++ controller/acceptance/util.ts
@@ -0,0 +1,7 @@
+export { uploadAcceptanceFiles } from "../integration/util";
+
+// Shared functions for Acceptance Tests
+export const ACCEPTANCE_AWS_PERMISSIONS: boolean = process.env.ACCEPTANCE_AWS_PERMISSIONS?.toLowerCase() === "true";
+
+// Beanstalk <SYSTEM_NAME>_<SERVICE_NAME>_URL
+export const integrationUrl = "http://" + (process.env.BUILD_APP_URL || `localhost:${process.env.PORT || "8081"}`);

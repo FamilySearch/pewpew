ac97a7da3a843b9f4c40e80f1758fb3492f6fd65
blonde-mike <macrowther7+git@gmail.com>
2025-01-30 09:02:37 -0700

    Updated the vars section with buttons rather than checkboxes (#287)

---
+++ guide/results-viewer-react/src/components/Icons/DeleteIcon.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const DeleteIcon = (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
+    return <svg
+        width="800px"
+        height="800px"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <path
+            d="M7 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2h4a1 1 0 1 1 0 2h-1.069l-.867 12.142A2 2 0 0 1 17.069 22H6.93a2 2 0 0 1-1.995-1.858L4.07 8H3a1 1 0 0 1 0-2h4V4zm2 2h6V4H9v2zM6.074 8l.857 12H17.07l.857-12H6.074zM10 10a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1z"
+            fill="white"
+        />
+    </svg>;
+};

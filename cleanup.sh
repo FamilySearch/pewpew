3f2412abcd99e3e0a10919d23b656ea6c4793dda
Trevor McMaster <tkmcmaster@users.noreply.github.com>
2026-01-06 13:35:57 -0700

    Updated the gitignore and cleanup scripts

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,8 @@ set -x # Echo commands
 rm fs_home.yaml
 rm integration.json
 rm test-epoch*.json
+rm tests/test-epoch*.json
+rm tests/stats-integration*.json
 rm examples/*.out
 rm examples/search-*.json
 rm examples/stats-*.json
